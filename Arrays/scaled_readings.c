5c639900e85ac2a3280154b78d815bc00be5610b
Dexter Niles <dexter.niles5@gmail.com>
2026-01-05 18:38:56 -0500

    getting the hang of arrays in functions

---
+++ Arrays/scaled_readings.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void scale_array(int arr[], int size, int factor) {
+    for (int i = 0; i < size; i++) {
+        arr[i] *= factor;
+    }
+}
+
+void print_array(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d\n", arr[i]);
+    }
+}
+
+int sum_array(int arr[], int size) {
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    } return sum;
+}
+
+int main(void) {
+    int readings[] = {2, 4, 6, 8, 10};
+    int size  = sizeof(readings) / sizeof(readings[0]);
+    int original_total = sum_array(readings, size);
+
+    print_array(readings, size);
+    printf("Original Sum: %d\n", original_total);
+    
+    scale_array(readings, size, 2);
+    print_array(readings, size);
+
+    int scaled_total = sum_array(readings, size);
+
+    printf("Scaled Sum: %d\n", scaled_total);
+
+    return 0;
+}
